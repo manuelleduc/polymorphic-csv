56468a89e306a9a827afba4ccb092feecc8dda73
Yannick NAMOUR <yannick@bash.home>
2018-04-11 20:57:05 +0200

    11/04

--- polymorphic.csv/xtend-gen/polymorphic/generator/GeneratorCollection.java
+++ polymorphic.csv/xtend-gen/polymorphic/generator/GeneratorCollection.java
@@ -3,6 +3,7 @@ package polymorphic.generator;
 import java.util.Map;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import polymorphic.generator.csv.ApacheCommonCsvGenerator;
+import polymorphic.generator.csv.BashCsvGenerator;
 import polymorphic.generator.csv.ICsvGenerator;
 import polymorphic.generator.csv.JavaCsvGenerator;
 import polymorphic.generator.csv.PythonCsvGenerator;
@@ -15,16 +16,14 @@ public class GeneratorCollection {
   private Map<String, ICsvGenerator> map = CollectionLiterals.<String, ICsvGenerator>newHashMap();
   
   public Map<String, ICsvGenerator> getMap() {
-    Map<String, ICsvGenerator> _xblockexpression = null;
-    {
-      JavaCsvGenerator _javaCsvGenerator = new JavaCsvGenerator();
-      this.map.put("java", _javaCsvGenerator);
-      ApacheCommonCsvGenerator _apacheCommonCsvGenerator = new ApacheCommonCsvGenerator();
-      this.map.put("commons", _apacheCommonCsvGenerator);
-      PythonCsvGenerator _pythonCsvGenerator = new PythonCsvGenerator();
-      this.map.put("python", _pythonCsvGenerator);
-      _xblockexpression = this.map;
-    }
-    return _xblockexpression;
+    JavaCsvGenerator _javaCsvGenerator = new JavaCsvGenerator();
+    this.map.put("java", _javaCsvGenerator);
+    ApacheCommonCsvGenerator _apacheCommonCsvGenerator = new ApacheCommonCsvGenerator();
+    this.map.put("commons", _apacheCommonCsvGenerator);
+    PythonCsvGenerator _pythonCsvGenerator = new PythonCsvGenerator();
+    this.map.put("python", _pythonCsvGenerator);
+    BashCsvGenerator _bashCsvGenerator = new BashCsvGenerator();
+    this.map.put("bash", _bashCsvGenerator);
+    return this.map;
   }
 }
